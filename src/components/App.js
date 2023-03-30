bc116738634bba097d33f026bed388f8e22a839a
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-03-30 09:25:29 +0200

    state-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Counter } from "./Counter";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 
@@ -8,6 +9,7 @@ export default class App extends React.Component {
       <div>
         <Hello />
         <Welcome name ="John" age={29}/>
+        <Counter/>
 
       </div>
     );
