279b21f66702f127197e646e718e0d3aaf9e8edd
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-06 15:47:42 +0200

    exercise form-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import ClickCounter from "./ClickCounter";
 import Hello from "./Hello";
-import Welcome from "./Welcome";
 import ClickTracker from "./ClickTracker";
+import InteractiveWelcome from "./InteractiveWelcome";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={30} />               
+        <InteractiveWelcome />            
         <ClickCounter />        
         <ClickTracker />
       </div>
---
+++ src/components/InteractiveWelcome.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+export default class InteractiveWelcome extends React.Component {
+  state = {
+    name: "",
+  };
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState((prevState) => ({
+      name: value,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <input name="name" value={this.state.name} onChange={this.handleInputChange} placeholder="What's your name?"/>
+        {this.state.name && <Welcome name={this.state.name} age="29" />}
+      </div>
+    );
+  }
+}
