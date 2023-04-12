3761fa8b72c5b9bb2e8ebd4fe87ccca441298bb3
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-12 15:16:31 +0200

    exercise context

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { TodoList } from "./TodoList";
+import { LanguageContext } from "./LanguageContext";
 
 export default class App extends Component {
-   render() {
+  state = {
+    language: "en",
+};
+
+handleLanguageChange = (event) =>{
+    this.setState({
+        language: event.target.value
+    })
+}
+
+render() {
     return (
-      <div>        
-        <TodoList/>
-        
-      </div>
+        <div>
+            <select value={this.state.language} onChange={this.handleLanguageChange}>
+                <option value="en">En</option>
+                <option value="it">It</option>
+                <option value="es">Es</option>
+                <option value="de">De</option>
+            </select>
+            <LanguageContext.Provider value={this.state.language}/>  
+                        
+        </div>
     );
-  }
+}
 }
