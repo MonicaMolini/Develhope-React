3a8f335e6c6b6fbc02326eb2831bd1a749d65a4f
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-12 12:25:31 +0200

    exercise form-3

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,12 +17,20 @@ export class Login extends React.Component{
         this.props.onLogin(this.state)
     }
 
+    handleReset = () =>{
+        this.setState({
+            username: "",
+            password: ""
+        })
+    }
+
     render(){
         return (
             <div>
                 <input name="username" onChange={this.handleChange} placeholder="Username"></input>
                 <input name="password" type="password" onChange={this.handleChange} placeholder="Password"></input>
                 <button type="submit" onClick={this.handleButton} disabled = {!this.state.username || !this.state.password}>Login</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         )
     }
