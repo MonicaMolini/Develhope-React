420fd0e1f51f113c332e46058d5862a7151e1831
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-12 12:12:56 +0200

    exercise form-2

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import ClickCounter from "./ClickCounter";
-import Hello from "./Hello";
-import ClickTracker from "./ClickTracker";
-import InteractiveWelcome from "./InteractiveWelcome";
+import { Login } from "./Login";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Hello />
-        <InteractiveWelcome />            
-        <ClickCounter />        
-        <ClickTracker />
+      <div>        
+        <Login onLog={this.onlogin}></Login>
+        
       </div>
     );
   }
---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export class Login extends React.Component{
+    state = {
+        username : "",
+        password: ""
+    }
+
+    handleChange = (event) =>{
+        const name = event.target.name;
+        this.setState({
+            [name]: event.target.value,
+        })
+    }
+
+    handleButton = ()=> {
+        this.props.onLog(this.state)
+    }
+
+    render(){
+        return (
+            <div>
+                <input name="username" onChange={this.handleChange} placeholder="Username"></input>
+                <input name="password" type="password" onChange={this.handleChange} placeholder="Password"></input>
+                <button onClick={this.handleButton} disabled = {!this.state.username || !this.state.password}>Login</button>
+            </div>
+        )
+    }
+}
