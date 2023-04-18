252f6f74dfe773112a2797ecb4bf82962a4c85e9
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-18 17:51:41 +0200

    exercise custom-hooks-4

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -2,11 +2,15 @@ import React from "react";
 import useGithubUser from "./hooks/useGithubUser";
 
 export default function GithubUser({username}) {
-  const {data} = useGithubUser(username)  
-   return (
-    <div>
-      <h1>{data.login}</h1>
-      <img src={data.avatar_url} alt="avatar" />
-    </div>
-  );
+  const { data, error, loading } = useGithubUser(username);
+  return(
+    <>        
+        {error && <div>{error}</div>}
+        {loading && <div>loading...</div>}
+        {data && <div>
+            <h2>{data.login}</h2>
+            <img src={data.avatar_url}></img>             
+            </div>}
+    </>
+)
 }
--- src/components/hooks/useGithubUser.js
+++ src/components/hooks/useGithubUser.js
@@ -2,15 +2,27 @@ import { useEffect, useState } from "react";
 
 export default function useGithubUser(username) {
   const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
 
-  useEffect(()=>{
-    fetch(`https://api.github.com/users/${username}`)
-        .then(response => response.json())
-        .then(result => setData(result))
-        .catch(error => console.log(error))
-},[username])
-  
-  return {  
-    data: data
-  };
+  async function getGitUser(){
+    setError(null)
+    setLoading(true)
+    try{
+        const response = await  fetch(`https://api.github.com/users/${username}`)
+        const json = await response.json()
+        setData(json)
+    }catch(error){
+        setError(error)
+        setData(null)
+    }finally{
+        setLoading(false)
+    }
+}
+useEffect(() => {
+  getGitUser(username);
+}, [username]);
+
+return { data, error, loading};
+
 }
