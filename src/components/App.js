4c4a63a3a53d11d11b33733319feec18fef9c48c
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-22 17:54:00 +0200

    exercise Route-2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Counter from "./Counter";
+import ShowGitHubUser from "./ShowGitHubUser";
 
 
 export default function App() {
   return (
     <div>
-      <Link to="/">Home</Link> - <Link to="/counter">Counter</Link>
+      <Link to="/">Home</Link> - <Link to="/counter">Counter</Link> - <Link to="/users/MonicaMolini">GitHub profile</Link>
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/users/:username" element={<ShowGitHubUser />} />
       </Routes>
     </div>
 
