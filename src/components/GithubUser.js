024a90450628d0c50d2be4dc50df3536d0bd27ce
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-22 19:52:38 +0200

    exercise swr-3

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -2,15 +2,16 @@ import React from "react";
 import useGithubUser from "./hooks/useGithubUser";
 
 export default function GithubUser({username}) {
-  const { data, error } = useGithubUser(username);
+  const { data, error, refetchdata} = useGithubUser(username);
   return(
     <>        
         {error && <div>{error}</div>}
         {!data && !error && <div>loading...</div>}
-        {data && <div>
+        {data && <><div>
             <h2>{data.login}</h2>
             <img src={data.avatar_url}></img>             
-            </div>}
+            </div>
+            <button onClick={refetchdata}>Refetch</button> </>}
     </>
 )
 }
--- src/components/hooks/useGithubUser.js
+++ src/components/hooks/useGithubUser.js
@@ -3,6 +3,9 @@ const fetcher = (url) => fetch(url).then(result => result.json())
 
 
 export default function useGithubUser(username) {
-  const {data, error} = useSWR( username ? `https://api.github.com/users/${username}`: null, fetcher);
-  return {data, error};
+  const {data, error, mutate} = useSWR( username ? `https://api.github.com/users/${username}`: null, fetcher);
+  const refetchdata = () => {
+    mutate();
+  };
+  return {data, error, refetchdata};
 }
