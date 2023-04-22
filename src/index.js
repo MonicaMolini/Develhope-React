d3d66b4a277fc656e2259d1076d4fbdd251e4f9c
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-04-22 17:45:26 +0200

    exercise Route-1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import {createRoot} from 'react-dom/client'
-import App  from './components/App'
+import { createRoot } from "react-dom/client";
+import Root from "./Root";
 
-const root = createRoot(document.getElementById('root'))
-
-root.render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(<Root />);
